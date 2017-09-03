5c4f5cd1d06fcd7e62efb25036ba42eedbce9ff4
Max Berger <maxamillianberger@gmail.com>
2017-09-03 12:59:53 -0400

    Change default port to 8081

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.port || 8081;
 const root = path.join(__dirname, '/..');
 
 app.use(express.static(root));
